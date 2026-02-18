64d4a74369687ed3b172b9c7b206f9984acf0784
Simon Hetu <simonhetu@gmail.com>
2026-02-17 19:27:36 -0500

    remove unused varaibles

--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import type { Incident } from "../components/services/incidents";
 import type { Pdq } from "../components/services/pdq";
-import logo from "../assets/crimevision_logo_10.png";
-
 
 type Filters = {
   years: Set<number>;
